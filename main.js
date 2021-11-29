8cc8ece2c38eaf2066b8edc05de3e2fd9a3a730e
meepma <80076185+meepma@users.noreply.github.com>
2021-11-29 18:58:24 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -13,6 +13,8 @@ function setup(){
     canvas = createCanvas(300,300);
 canvas.center();
 background("white");
+canvas.mouseReleased(classifyCanvas);
+synth=window.speechSynthesis;
 }
 
 function updateCanvas() {
@@ -24,6 +26,11 @@ function updateCanvas() {
 }
 
 function draw() {
+    strokeWeight(12);
+    stroke("grey");
+    if(mouseIsPressed){
+        line(pmouseX,pmouseY,mouseX,mouseY);
+    }
     check_sketch();
     if (drawn_sketch == sketch) {
         answer_holder = "set"
@@ -47,3 +54,22 @@ function check_sketch() {
 
     }
 }
+function preload(){
+    classifier=ml5.imageClassifier("DoodleNet");
+
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas,gotResult);
+}
+function gotResult(error,results){
+     if(error){
+         console.error(error);
+     }
+     console.log(results);
+     document.getElementById("label").innerHTML="Label: "+results[0].label;
+     document.getElementById("confidence").innerHTML="Confidence: "+Math.round(results[0].confidence*100);
+     utterThis=new SpeechSynthesisUtterance(results[0].label);
+     synth.speak(utterThis);
+    }
+
